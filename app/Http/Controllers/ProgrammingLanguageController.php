6061b2153346066d390693e1d9db3a6ea0d82a96
MaksymRosokha <maksimrosoha6@gmail.com>
2023-02-03 12:09:02 +0200

    Added validation when entering a non-existing programming language id.

--- app/Http/Controllers/ProgrammingLanguageController.php
+++ app/Http/Controllers/ProgrammingLanguageController.php
@@ -6,13 +6,13 @@ use App\Models\ProgrammingLanguage;
 use Illuminate\Http\Request;
 
 /**
- * This class is controller for programming languages page
+ * This class is controller for programming languages
  */
 class ProgrammingLanguageController extends Controller
 {
 
     /**
-     * This method opens programming languages page
+     * Shows page of programming languages.
      *
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
@@ -21,13 +21,13 @@ class ProgrammingLanguageController extends Controller
     }
 
     /**
-     * This function shows one programming language page.
+     * Shows one programming language page by id.
      *
      * @param int $id programming language id
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
-    public function showOneLanguage(int $id){
-        $programmingLanguage = ProgrammingLanguage::query()->find($id);
+    public function showOneLanguage($id){
+        $programmingLanguage = ProgrammingLanguage::query()->findOrFail($id);
         return view('programmingLanguages.programmingLanguage', ['programmingLanguage' => $programmingLanguage]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\IndexController::class, 'index'])->name('main');
+
 Route::get('/programming_languages', [\App\Http\Controllers\ProgrammingLanguageController::class, 'showPage'])
     ->name('programming_languages');
 Route::get('/programming_language/{id}', [\App\Http\Controllers\ProgrammingLanguageController::class, 'showOneLanguage'])
@@ -23,12 +24,12 @@ Route::get('/programming_language/{id}', [\App\Http\Controllers\ProgrammingLangu
 Route::middleware('guest')->group(function () {
     Route::get('/login', [\App\Http\Controllers\AuthController::class, 'showLoginForm'])
         ->name('login');
-    Route::post('/login/process', [\App\Http\Controllers\AuthController::class, 'loginUser'])
+    Route::post('/login', [\App\Http\Controllers\AuthController::class, 'loginUser'])
         ->name('login_process');
 
     Route::get('/signup', [\App\Http\Controllers\AuthController::class, 'showSignUpForm'])
         ->name('signUp');
-    Route::post('/signup/process', [\App\Http\Controllers\AuthController::class, 'registerUser'])
+    Route::post('/signup', [\App\Http\Controllers\AuthController::class, 'registerUser'])
         ->name('registerUser');
 });
 
