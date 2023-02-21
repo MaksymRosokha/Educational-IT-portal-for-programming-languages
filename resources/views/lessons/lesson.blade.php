f57ea0f9f00b6be4814aabb7a3aac38b57df117a
MaksymRosokha <maksimrosoha6@gmail.com>
2023-02-21 20:30:46 +0200

    Created style files for a lesson.

--- resources/views/lessons/lesson.blade.php
+++ resources/views/lessons/lesson.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.app')
 
+@section('css_file')
+    /css/lesson.css
+@endsection()
+
 @section('title')
     {{ $lesson->title }}
 @endsection
 
 @section('content')
-    <div>
-        <h2>{{ $lesson->title }}</h2>
-        <p>{!! $lesson->content !!}</p>
+    <div class="lesson">
+        <h2 class="lesson__title">{{ $lesson->title }}</h2>
+        <p class="lesson__content">{!! $lesson->content !!}</p>
     </div>
 @endsection
