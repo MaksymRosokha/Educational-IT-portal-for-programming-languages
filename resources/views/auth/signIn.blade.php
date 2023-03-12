dda0f4c8adeaab04554e9db0afcfc01f8d7168d1
MaksymRosokha <maksimrosoha6@gmail.com>
2023-03-12 21:20:43 +0200

    Styled the sign in page.

--- resources/views/auth/signIn.blade.php
+++ resources/views/auth/signIn.blade.php
@@ -4,24 +4,50 @@
     Вхід
 @endsection
 
-@section('content')
-    <section>
-        <h2>Вхід</h2>
-        <form action="{{ route('loginProcess') }}" method="POST">
-            @csrf
+@section('styles')
+    <link rel="stylesheet" href="/css/signIn.css">
+@endsection
 
-            @error('email')
-            <p>{{ $message }}</p>
-            @enderror
-            <input type="text" name="email" placeholder="Введіть email">
-            <br>
-            @error('password')
-            <p>{{ $message }}</p>
-            @enderror
-            <input type="password" name="password" placeholder="Введіть пароль">
-            <br>
-            <button type="submit">Увійти</button>
-        </form>
-        <a href="{{ route('signUp') }}">Реєстрація</a>
+@section('content')
+    <section class="sign-in">
+        <div class="sign-in__wrapper">
+            <h2 class="sign-in__title">Вхід</h2>
+            <form class="sign-in__form sign-in-form" action="{{ route('loginProcess') }}" method="POST">
+                @csrf
+
+                @error('email')
+                <p class="sign-in-form__error">{{ $message }}</p>
+                @enderror
+                <label for="sign-in-form__input-field" class="sign-in-form__label">Email:</label>
+                <input class="sign-in-form__input-field" type="email" name="email" placeholder="Введіть ваш email"
+                       required maxlength="320" minlength="3">
+                @error('password')
+                <p class="sign-in-form__error">{{ $message }}</p>
+                @enderror
+                <label for="sign-in-form__input-field" class="sign-in-form__label">Пароль:</label>
+                <input class="sign-in-form__input-field" type="password" name="password"
+                       placeholder="Введіть ваш пароль" required minlength="6" maxlength="100">
+                <button class="sign-in-form__do-sign-in" type="submit">Увійти</button>
+            </form>
+            <div class="sign-in__additional-links">
+                <div class="additional-link sign-in__forgot-password">
+                    <label class="additional-link__label" for="">Забули пароль?</label>
+                    <a class="additional-link__link" href="#">Відновити</a>
+                </div>
+                <div class="additional-link sign-in__sign-up">
+                    <label class="additional-link__label" for="">Не маєте акаунту?</label>
+                    <a class="additional-link__link" href="{{ route('signUp') }}">Реєстрація</a>
+                </div>
+            </div>
+        </div>
     </section>
+
+    <script>
+        let items = document.querySelectorAll('.sign-in-form__input-field');
+        items.forEach(input => {
+            input.addEventListener('select', () => {
+                input.style.backgroundColor = 'transparent';
+            });
+        });
+    </script>
 @endsection
