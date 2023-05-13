9f9fc2190a1f8fa3edc65e5b390d60a65f73fa0c
MaksymRosokha <maksimrosoha6@gmail.com>
2023-05-13 12:00:39 +0300

    Filled in the footer and stylised it.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,65 @@
 <div class="footer">
     <div class="footer__container">
-
+        <span class="footer__copyright">©Інформація даного вебсайту захищена авторськими правами 2023</span>
+        <div class="footer__info-blocks">
+            <div class="footer__info-block info-block">
+                <span class="info-block__title">Контактна інформація</span>
+                <div class="info-block__information">
+                    <div class="info-block__sub-block sub-block">
+                        <a class="sub-block__link"
+                           href="tel:+380969409811">
+                            <img class="sub-block__icon"
+                                 src="{{ asset('storage/images/footer/phone.png') }}"
+                                 alt="Іконка телефону">
+                            +380969409811
+                        </a>
+                    </div>
+                    <div class="info-block__sub-block sub-block">
+                        <a class="sub-block__link"
+                           href="mailto:c.rosokha.maksym@student.uzhnu.edu.ua">
+                            <img class="sub-block__icon"
+                                 src="{{ asset('storage/images/footer/email.png') }}"
+                                 alt="Іконка електронної пошти">
+                            c.rosokha.maksym@student.uzhnu.edu.ua
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__info-block info-block">
+                <span class="info-block__title">Соціальні мережі</span>
+                <div class="info-block__information">
+                    <div class="info-block__sub-block sub-block">
+                        <a class="sub-block__link"
+                           href="https://www.instagram.com/r_maks_v/"
+                           target="_blank">
+                            <img class="sub-block__icon"
+                                 src="{{ asset('storage/images/footer/instagram.png') }}"
+                                 alt="Іконка Instagram">
+                            Instagram
+                        </a>
+                    </div>
+                    <div class="info-block__sub-block sub-block">
+                        <a class="sub-block__link"
+                           href="https://t.me/maks_v_r"
+                           target="_blank">
+                            <img class="sub-block__icon"
+                                 src="{{ asset('storage/images/footer/telegram.png') }}"
+                                 alt="Іконка Telegram">
+                            Telegram
+                        </a>
+                    </div>
+                    <div class="info-block__sub-block sub-block">
+                        <a class="sub-block__link"
+                           href="https://www.facebook.com/profile.php?id=100009106858433"
+                           target="_blank">
+                            <img class="sub-block__icon"
+                                 src="{{ asset('storage/images/footer/facebook.png') }}"
+                                 alt="Іконка Facebook">
+                            Facebook
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
